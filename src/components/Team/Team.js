92cd1e9addc378831efd5a98a88d7506e39ac41b
tristan-sch <tristan@Tristans-iMac.local>
2022-04-28 22:24:35 +0000

    updated team.js

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./Team.module.scss";
 
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight, FaChevronLeft, FaChevronDown } from "react-icons/fa";
 
 import Image from "next/image";
 import arrowDownYellow from "../../../public/arrowDownYellow.png";
@@ -119,22 +119,22 @@ export default function Team({ menus, team }) {
             <FaChevronRight className="rightArrow" onClick={nextSlide} />
           </div>
         </div>
-        {menus.nodes.map((menu, index) => (
-          <div key={index} className={styles.ctaGrid}>
-            <h3>{menu.menuItems.edges[2].node.label}</h3>
-          </div>
-        ))}
         <div className={styles.arrowDownGrid}>
-          <Link href="#contact">
-            <a>
-              <Image
-                src={arrowDownYellow}
-                alt="Arrow down"
-                width={50}
-                height={50}
-              />
-            </a>
-          </Link>
+          {menus.nodes.map((menu) => (
+            <h4 key={menu.menuItems.edges[2].node.id}>
+              {menu.menuItems.edges[2].node.label}
+            </h4>
+          ))}
+          {menus.nodes.map((menu) => (
+            <Link
+              key={menu.menuItems.edges[2].node.id}
+              href={menu.menuItems.edges[2].node.path}
+            >
+              <a className={styles.arrow}>
+                <FaChevronDown />
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
