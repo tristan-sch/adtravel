92fddaa6c563d73016553d3e51e3e6a3d9caff02
tristan-sch <tristan@Tristans-iMac.local>
2022-06-21 22:15:09 +0000

    trying again

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   reactStrictMode: true,
-  module: {
-    rules: [
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ["@svgr/webpack"],
-      },
-    ],
-  },
   images: {
     domains: ["new.adtravel.is"],
   },
