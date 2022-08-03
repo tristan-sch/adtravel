81de83095ff1f73cfe38c1964d3df5fd028a37fe
tristan-sch <tristan@Tristans-iMac.local>
2022-08-03 16:51:43 +0000

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,10 @@ import React from "react";
 import tw from "twin.macro";
 import Image from "next/image";
 import Link from "next/link";
-
-import { backgroundImage } from "../../public/404.png";
+import backgroundImage from "../../public/404.png";
 
 const Container = tw.div`relative -mt-8 bg-center bg-cover h-screen min-h-144`;
 const PrimaryAction = tw.button`rounded-lg px-8 py-3 mt-10 text-sm sm:text-base sm:mt-16 sm:px-8 sm:py-4 bg-gray-100 font-bold shadow transition duration-300 bg-primary-dark text-gray-100 hocus:bg-secondary-dark hocus:text-gray-200 focus:outline-none focus:shadow-outline`;
-const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-black opacity-50`;
 const HeroContainer = tw.div`z-20 relative px-6 sm:px-8 mx-auto h-full flex flex-col`;
 const Content = tw.div`px-4 md:-mt-20 flex flex-1 flex-col justify-center items-center`;
 const Heading = tw.h1`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl font-bold text-gray-100 leading-snug  -mt-24 sm:mt-0 font-nunito`;
@@ -15,7 +13,6 @@ const Heading = tw.h1`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl f
 const FourOhFour = () => {
   return (
     <Container>
-      <OpacityOverlay />
       <Image
         alt="alt Text"
         src={backgroundImage}
