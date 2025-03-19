7d381db2cab52f50428635a310aaa4792e4a0746
tristan-sch <schmale.tristan@gmail.com>
2025-03-19 18:53:06 +0100

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['wp.adtravel.is'],
+    domains: ["wp.adtravel.is"],
+    // TODO: fix this
+    // Temporary fix until a solution is found for the image optimization issue
+    unoptimized: true,
   },
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
